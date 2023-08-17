078456d21d02cb8c5d51836b86da5c594e469990
Azamat <sabmile@gmail.com>
2023-08-17 11:26:50 +0600

    add Permissions policy

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -1,6 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Permissions-Policy" content="interest-cohort=()">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <!-- <link rel="stylesheet" href= "{{ asset('css/main.css') }}"> -->
     <title>{{__('content.title')}}</title>
