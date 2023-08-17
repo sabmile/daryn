dc604a95b31592f893831b8a6f9567eb3b063777
Azamat <sabmile@gmail.com>
2023-08-17 16:01:00 +0600

    split page to 2 columns

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -1,3 +1,18 @@
 <main class="flex-grow-1 py-3">
-    @yield('content')    
+    <div class="cotainer">
+        <div class="row">
+            <div class="col">
+                @yield('content')
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 d-none d-sm-block">
+                <aside>
+                    <ul class="nav d-flex flex-column align-items-center">
+                        <li><a class="nav-link" href="{{ route('site.schedule') }}">{{__('content.nav.schedule')}}</a></li>
+                        <li><a class="nav-link" href="{{ route('site.about') }}">{{__('content.nav.history')}}</a></li>
+                        <li><a class="nav-link" href="{{ route('site.galeryVideo') }}">{{__('content.nav.video')}}</a></li>
+                    </ul>
+                </aside>
+            </div>
+        </div>
+    </div>
 </main>
