1da7f525ac8cb8fc3837c2cfd2d00681471eaf8b
Azamat <sabmile@gmail.com>
2023-08-18 10:12:09 +0600

    set content in about component

---
+++ resources/views/components/about.blade.php
@@ -0,0 +1,15 @@
+<div class="container-fluid">
+    <div class="row">
+        <h3>{{__('content.about.history.row-1-name')}}</h3>
+        <p>{{__('content.about.history.row-1-content')}}</p>
+
+        <h3>{{__('content.about.history.row-2-name')}}</h3>
+        <p>{{__('content.about.history.row-2-content')}}</p>
+
+        <h3>{{__('content.about.history.row-3-name')}}</h3>
+        <p>{{__('content.about.history.row-3-content')}}</p>
+
+        <h3>{{__('content.about.history.row-4-name')}}</h3>
+        <p>{{__('content.about.history.row-4-content')}}</p>
+    </div>
+</div>
