4c63db2fd5d48db1300fc5113ab90dc6490a831e
Azamat <sabmile@gmail.com>
2023-11-14 14:17:11 +0600

    add data a new view to controller

---
+++ resources/views/site/pedagogical-support.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.base')
+
+@section('content')
+<div class="pedagogical-support">
+	<ul class="pedagogical-support-list">
+		<li class="pedagogical-support-list-item">
+			<a href="{{ URL('/assets/docs/Ата-аналарды қолдау орталығы.pdf') }}" target="_blank">Ата-аналарды қолдау орталығы</a>
+		</li>
+	</ul>
+</div>
+@endsection
